61883d23f085f3b6ab7b12c4f5ec14d43afc6d62
soft1318 <soft1318@naver.com>
2024-02-04 15:51:51 +0900

    소스 보완 수정

--- src/main/java/kopo/poly/dto/NoticeDTO.java
+++ src/main/java/kopo/poly/dto/NoticeDTO.java
@@ -6,12 +6,6 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-/**
- * lombok은 코딩을 줄이기 위해 @어노테이션을 통한 자동 코드 완성기능임
- *
- * @Getter => getter 함수를 작성하지 않았지만, 자동 생성
- * @Setter => setter 함수를 작성하지 않았지만, 자동 생성
- */
 @Getter
 @Setter
 @ToString
